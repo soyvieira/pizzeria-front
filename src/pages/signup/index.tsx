6cec249fe591c0bf3d144293af631d4e3b612ba4
soyvieira <soyvieira8@gmail.com>
2023-05-18 16:20:41 -0300

    Sign up page

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -1,24 +1,29 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import styles from '../../styles/home.module.scss';
+import styles from '../../../styles/home.module.scss';
 
-import {Input} from '../components/ui/Input';
-import { Button } from '../components/ui/Button';
+import {Input} from '../../components/ui/Input';
+import { Button } from '../../components/ui/Button';
 
-import logoImg from '../../public/logo.svg';
+import logoImg from '../../../public/logo.svg';
 
 import Link from 'next/link';
 
-export default function Home() {
+export default function SignUp() {
   return (
     <>
     <Head>
-      <title>Pizzeria</title>
+      <title>Register here - Pizzeria</title>
     </Head>
     <div className={styles.containerCenter}>
       <Image src={logoImg} alt="Logo Pizzeria" />
       <div className={styles.login}>
+        <h1>Register here</h1>
         <form>
+          <Input
+            placeholder='Your name'
+            type='text'
+          />
           <Input
             placeholder='Email'
             type='text'
@@ -31,12 +36,12 @@ export default function Home() {
             type='submit'
             loading={false}
           >
-            Log in
+            Sign up
           </Button>
         </form>
 
-        <Link href="/signup">
-          <p className={styles.text}> Don't have an account? Register here</p>
+        <Link href="/">
+          <p className={styles.text}> Do you already have an account? Log in here</p>
         </Link>
 
       </div>
